6480b642f7a41b7ecac91ede10d2057ba5a494ca
Hao-Chih Huang <83774247+He6s@users.noreply.github.com>
2021-12-07 10:11:38 -0800

    Update FSFTBuffer.java

--- src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
+++ src/main/java/cpen221/mp3/fsftbuffer/FSFTBuffer.java
@@ -1,5 +1,8 @@
 package cpen221.mp3.fsftbuffer;
 
+import java.util.*;
+
+
 public class FSFTBuffer<T extends Bufferable> {
 
     /* the default buffer size is 32 objects */
@@ -8,10 +11,16 @@ public class FSFTBuffer<T extends Bufferable> {
     /* the default timeout value is 3600s */
     public static final int DTIMEOUT = 3600;
 
-    /* TODO: Implement this datatype */
+    private final int capacity;
+    private final int timeout;
+    private final List<T> stale_time_list = new ArrayList<>();
+    private final List<T> recently_used_list = new ArrayList<>();
+    private final List<Long> stale_time = new ArrayList<>();
+    private final List<Long> recently_used_time = new ArrayList<>();
 
     /**
-     * Create a buffer with a fixed capacity and a timeout value.
+     * Create a buffer with a fixe
+     * d capacity and a timeout value.
      * Objects in the buffer that have not been refreshed within the
      * timeout period are removed from the cache.
      *
@@ -20,7 +29,8 @@ public class FSFTBuffer<T extends Bufferable> {
      *                 be in the buffer before it times out
      */
     public FSFTBuffer(int capacity, int timeout) {
-        // TODO: implement this constructor
+        this.capacity = capacity;
+        this.timeout = timeout;
     }
 
     /**
@@ -35,9 +45,33 @@ public class FSFTBuffer<T extends Bufferable> {
      * If the buffer is full then remove the least recently accessed
      * object to make room for the new object.
      */
-    public boolean put(T t) {
-        // TODO: implement this method
-        return false;
+    public synchronized boolean put(T t) {
+        check();
+
+        if (stale_time_list.size() > capacity) {
+            return false;
+        }
+
+        // remove the least recently accessed object from the buffer
+        else if (stale_time_list.size() == capacity) {
+            for (int stale_index = 0; stale_index < stale_time_list.size(); stale_index++) {
+                if (stale_time_list.get(stale_index).equals(recently_used_list.get(0))) {
+                    stale_time_list.remove(stale_index);
+                    stale_time.remove(stale_index);
+                    break;
+                }
+            }
+
+            recently_used_time.remove(0);
+            recently_used_list.remove(0);
+        }
+
+        stale_time_list.add(t);
+        stale_time.add(System.currentTimeMillis());
+        recently_used_time.add(System.currentTimeMillis());
+        recently_used_list.add(t);
+
+        return true;
     }
 
     /**
@@ -45,12 +79,32 @@ public class FSFTBuffer<T extends Bufferable> {
      * @return the object that matches the identifier from the
      * buffer
      */
-    public T get(String id) {
-        /* TODO: change this */
+    public synchronized T get(String id) throws IllegalAccessException {
+        check();
+
+        // renew recently used time
+        for (int t_index = 0; t_index < recently_used_list.size(); t_index++) {
+            if (recently_used_list.get(t_index).id().equals(id)) {
+                recently_used_list.add(recently_used_list.get(t_index));
+                recently_used_list.remove(t_index);
+
+                recently_used_time.remove(stale_time.get(t_index));
+                recently_used_time.add(System.currentTimeMillis());
+
+                break;
+            }
+        }
+
+        for (T t_index : stale_time_list) {
+            if (t_index.id().equals(id)) {
+                return t_index;
+            }
+        }
+
+        throw new IllegalAccessException();
         /* Do not return null. Throw a suitable checked exception when an object
             is not in the cache. You can add the checked exception to the method
             signature. */
-        return null;
     }
 
     /**
@@ -61,9 +115,39 @@ public class FSFTBuffer<T extends Bufferable> {
      * @param id the identifier of the object to "touch"
      * @return true if successful and false otherwise
      */
-    public boolean touch(String id) {
-        /* TODO: Implement this method */
-        return false;
+    public synchronized boolean touch(String id) {
+        check();
+
+        boolean truth_value = false;
+
+        // update stale time
+        for (int id_index = 0; id_index < stale_time_list.size(); id_index++) {
+            if (Objects.equals(stale_time_list.get(id_index).id(), id)) {
+                stale_time_list.add(stale_time_list.get(id_index));
+                stale_time_list.remove(id_index);
+
+                stale_time.remove(id_index);
+                stale_time.add(System.currentTimeMillis());
+
+                truth_value = true;
+
+                break;
+            }
+        }
+
+        // update recently used time
+        for (int id_index = 0; id_index < recently_used_time.size(); id_index++) {
+            if (Objects.equals(recently_used_list.get(id_index).id(), id)) {
+                recently_used_list.add(recently_used_list.get(id_index));
+                recently_used_list.remove(id_index);
+
+                recently_used_time.remove(id_index);
+                recently_used_time.add(System.currentTimeMillis());
+
+                break;
+            }
+        }
+        return truth_value;
     }
 
     /**
@@ -74,8 +158,54 @@ public class FSFTBuffer<T extends Bufferable> {
      * @param t the object to update
      * @return true if successful and false otherwise
      */
-    public boolean update(T t) {
-        /* TODO: implement this method */
-        return false;
+    public synchronized boolean update(T t) {
+        check();
+
+        boolean truth_value = false;
+
+        for (int t_index = 0; t_index < stale_time_list.size(); t_index++) {
+            if (t.id().equals(stale_time_list.get(t_index).id())) {
+                stale_time_list.add(t);
+                stale_time_list.remove(t_index);
+
+                stale_time.remove(t_index);
+                stale_time.add(System.currentTimeMillis());
+
+                truth_value = true;
+
+                break;
+            }
+        }
+
+        for (int t_index = 0; t_index < recently_used_time.size(); t_index++) {
+            if (t.id().equals(recently_used_list.get(t_index).id())) {
+                recently_used_list.add(t);
+                recently_used_list.remove(t_index);
+
+                recently_used_time.remove(t_index);
+                recently_used_time.add(System.currentTimeMillis());
+
+                break;
+            }
+        }
+
+        return truth_value;
+    }
+
+    private synchronized void check() {
+        for (int time_index = 0; time_index < stale_time.size(); time_index++) {
+            if (System.currentTimeMillis() - stale_time.get(time_index) > timeout * 1000L) {
+                for (int every_RU = 0; every_RU < recently_used_time.size(); every_RU++) {
+                    if (recently_used_list.get(every_RU).equals(stale_time_list.get(time_index))) {
+                        recently_used_list.remove(every_RU);
+                        recently_used_time.remove(every_RU);
+                        break;
+                    }
+                }
+
+                stale_time.remove(stale_time.get(time_index));
+                stale_time_list.remove(stale_time_list.get(time_index));
+            }
+        }
     }
 }
